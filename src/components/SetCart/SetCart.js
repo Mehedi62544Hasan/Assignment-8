5ab87bfe51bbf9643d955bc2d6abf3b72518e9c7
Mehedi62544Hasane <mehedi62544hasan@gmail.comm>
2022-09-29 12:12:04 +0100

    fourth commit

--- src/components/SetCart/SetCart.js
+++ src/components/SetCart/SetCart.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SetCart.css'
 // import img '../../../public/DSC_0009~2.JPG'
 
 const SetCart = (props) => {
+    const [breaks, setBreaks] = useState(0)
     console.log(props)
+    const breakTime =  (props) =>{
+        setBreaks(props)
+    }
     return (
-        <div>
+        <div className='container'>
             <div className='user'>
-                <img src= "" alt="" />
+                 <h1>img</h1>
                 <h3>Mehedi Hasan</h3>
+                <p>Junior Web Developer</p>
+                <p>Rangamati Govt Collage</p>
             </div>
             <div>
                 <div>
@@ -18,13 +24,19 @@ const SetCart = (props) => {
                 <div></div>
                 <div></div>
             </div>
-            <div>
-                {/* ass a break */}
+            <div className='break'>
+                <h4>Add a Break</h4>
+                <div>
+                    <p onClick={() =>breakTime(10)}>10m</p>
+                    <p onClick={() =>breakTime(20)}>20m</p>
+                    <p onClick={() =>breakTime(30)}>30m</p>
+                    <p onClick={() =>breakTime(40)}>40m</p>
+                </div>
             </div>
             <div>
                 <h4>Exercise Details</h4>
-                <h4>Exercise time: {props.times}</h4>
-                <h4>Break time: </h4>
+                <h4>Exercise time: {props.times} minit</h4>
+                <h4>Break time: {breaks} minit</h4>
              </div>
         </div>
     );
